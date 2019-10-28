566c6ce902ac9537843c38a03345a2f22c1fd749
Melanol <callmesolis@gmail.com>
2019-10-29 04:51:41 +0500

    Path-finding bug fixes

--- planet_eleven.py
+++ planet_eleven.py
@@ -450,26 +450,13 @@ class Unit(pyglet.sprite.Sprite):
 
     def move(self, destination):
         # Called once by RMB or when a unit is created
-        destination_x, destination_y = destination[0], destination[1]
-        print('self.x =', self.x, 'self.y =', self.y)
-        print('destination_x =', destination_x, 'destination_y =', destination_y)
         # Not moving: same coords
-        if self.x == destination_x and self.y == destination_y:
+        if self.x == destination[0] and self.y == destination[1]:
             self.destination_reached = True
             return
         # Moving or just rotating
         self.destination_reached = False
-        self.destination_x, self.destination_y = destination_x, destination_y
-        if not self.flying:
-            # Find closest empty coord between unit and destination
-            if ground_pos_coords_dict[(destination_x, destination_y)]:
-                diff_x = self.x - self.destination_x
-                diff_y = self.y - self.destination_y
-                angle = math.atan2(diff_y, diff_x)  # Rad
-                d_angle = math.degrees(angle)
-                rounded_d_angle = 45 * round(d_angle / 45)
-
-
+        self.destination_x, self.destination_y = destination[0], destination[1]
         # Rotation without movement for workers
         if isinstance(self, Builder):
             diff_x = self.destination_x - self.x
@@ -478,9 +465,45 @@ class Unit(pyglet.sprite.Sprite):
             self.rotation = -math.degrees(angle) + 90
             self.shadow.rotation = -math.degrees(angle) + 90
 
+        if not self.flying:
+            # Find closest empty coord between unit and destination
+            if ground_pos_coords_dict[(destination[0], destination[1])]:
+                diff_x = self.x - self.destination_x
+                diff_y = self.y - self.destination_y
+                angle = math.atan2(diff_y, diff_x)  # Rad
+                d_angle = math.degrees(angle)
+                rounded_d_angle = 45 * round(d_angle / 45)
+                if rounded_d_angle <= 0:
+                    rounded_d_angle += 360
+                if rounded_d_angle == 360:
+                    dx, dy = POS_SPACE, 0
+                elif rounded_d_angle == 45:
+                    dx, dy = POS_SPACE, POS_SPACE
+                elif rounded_d_angle == 90:
+                    dx, dy = 0, POS_SPACE
+                elif rounded_d_angle == 135:
+                    dx, dy = -POS_SPACE, POS_SPACE
+                elif rounded_d_angle == 180:
+                    dx, dy = -POS_SPACE, 0
+                elif rounded_d_angle == 225:
+                    dx, dy = -POS_SPACE, -POS_SPACE
+                elif rounded_d_angle == 270:
+                    dx, dy = 0, -POS_SPACE
+                elif rounded_d_angle == 315:
+                    dx, dy = POS_SPACE, -POS_SPACE
+                print('rounded_d_angle =', rounded_d_angle)
+                print('dx =', dx, 'dy =', dy)
+                while True:
+                    if ground_pos_coords_dict[(self.destination_x, self.destination_y)] is None:
+                        break
+                    self.destination_x += dx
+                    self.destination_y += dy
+                    if self.x == self.destination_x and self.y == self.destination_y:
+                        return
+
         self.pfi = 0
         if not self.flying:
-            self.path = find_path((self.x, self.y), (destination_x, destination_y))
+            self.path = find_path((self.x, self.y), (self.destination_x, self.destination_y))
         else:
             pass
         target = self.path[self.pfi]
