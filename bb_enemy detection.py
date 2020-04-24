1173477274e39c0d4671ec6f3df3e19677529b07
Melanol <callmesolis@gmail.com>
2020-04-24 22:02:29 +0500

    bb_enemy_detection.py

--- bb_enemy detection.py
+++ bb_enemy detection.py
@@ -1,4 +1,4 @@
-rad_mat = [[(0, 0)]]
+rad_matx = [[(0, 0)]]
 max_rad = 10
 for r in range(1, max_rad + 1):
     rad_arr = []
@@ -23,27 +23,35 @@ for r in range(1, max_rad + 1):
     for _ in range(width - 2):
         rad_arr.append((x, y))
         y -= 1
-    rad_mat.append(rad_arr)
-print(rad_mat)
-arr_2_print = []
-for arr in rad_mat:
-    arr_2_print.append(len(arr))
-print(arr_2_print)
+    print('rad_arr =', rad_arr)
+    rad_matx.append(rad_arr)
+lengths = []
+for arr in rad_matx:
+    lengths.append(len(arr))
+print('arr_2_print =', lengths)
 
 rad_clipped = []
 rad = 0
-for arr in rad_mat:
+too_far = [[]]
+i = 0
+for arr in rad_matx:
     rad_arr = []
-    print('rad =', rad)
-    for coord in arr:
+    # print('rad =', rad)
+    print(i)
+    inner_arr = []
+    for coord in arr + too_far[i]:
         d = (coord[0] ** 2 + coord[1] ** 2) ** 0.5
         if d <= rad:
             rad_arr.append(coord)
+        else:
+            inner_arr.append(coord)
     rad_clipped.append(rad_arr)
+    too_far.append(inner_arr)
     rad += 1
+    i += 1
 
-print(rad_clipped)
-arr_2_print = []
+print('rad_clipped =', rad_clipped)
+lengths = []
 for arr in rad_clipped:
-    arr_2_print.append(len(arr))
-print(arr_2_print)
+    lengths.append(len(arr))
+print(lengths)
